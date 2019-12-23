215c72d8cc7ed9e526398b0ff801daf802f59f35
Diego Diaz <evoevo.diaz@gmail.com>
2019-12-23 12:50:16 -0600

    Probando push con script

--- smrt_cmmt.sh
+++ smrt_cmmt.sh
@@ -1,6 +1,15 @@
-smrt_cmmt(){
-	if [ -z "$2" ]
-		git add .
-		git commit -m "$1"
-		git push origin 
+branch_check(){
+    branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    delete=("$@")
+    message=${delete[0]}
+    files=${@/$message}
+    if [[ $files != "" ]]; then
+        for i in $files; do
+            git add $i
+        done
+    else
+         git add .
+    fi
+    git commit -m $message
+    git push origin $branch
 }
