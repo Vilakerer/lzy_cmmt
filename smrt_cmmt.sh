c98af88e58f4bb0aad7a1d0a1bb53a0821302e81
Diego Diaz <evoevo.diaz@gmail.com>
2019-12-23 18:33:29 -0600

    Mensaje de prueba

--- smrt_cmmt.sh
+++ smrt_cmmt.sh
@@ -1,15 +1,13 @@
 branch_check(){
     branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-    delete=("$@")
-    message=${delete[0]}
-    files=${@/$message}
-    if [[ $files != "" ]]; then
-        for i in $files; do
+    len=$#
+    if [[ $len > 1 ]]; then
+        for i in "${@:2}"; do
             git add $i
         done
     else
-         git add .
+        git add .
     fi
-    git commit -m $message
-    git push origin $branch
+    git commit -m "$1"
+    git push origin "$branch"
 }
